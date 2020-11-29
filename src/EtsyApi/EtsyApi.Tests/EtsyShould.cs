ad5ae008845a9f598e4d0ac354bf876b0ce79de8
Nathan Hollis <natehollis@gmail.com>
2020-11-29 11:05:08 -0600

    CI

--- src/EtsyApi/EtsyApi.Tests/EtsyShould.cs
+++ src/EtsyApi/EtsyApi.Tests/EtsyShould.cs
@@ -7,30 +7,34 @@ namespace EtsyApi.Tests
     // set credentials in Conventions.cs
     public class EtsyShould
     {
-        [Theory, Conventions]
-        public void Login(EtsyService.EtsyService etsyService)
-        {
-            var r = etsyService.Login();
+        //add your credentials and uncomment
+
+        //TODO: unit tests
+
+        //[Theory, Conventions]
+        //public void Login(EtsyService.EtsyService etsyService)
+        //{
+        //    var r = etsyService.Login();
 
             
-        }
+        //}
 
 
-        [Theory, Conventions]
-        public void GetOauthTokens(EtsyService.EtsyService etsyService)
-        {
-            var r = etsyService.GetOauthTokens();
+        //[Theory, Conventions]
+        //public void GetOauthTokens(EtsyService.EtsyService etsyService)
+        //{
+        //    var r = etsyService.GetOauthTokens();
 
 
-        }
+        //}
 
-        [Theory, Conventions]
-        public async Task Search(EtsyService.EtsyService etsyService)
-        {
-            var r = await etsyService.Search("monstera deliciosa", 100, 0, 1);
+        //[Theory, Conventions]
+        //public async Task Search(EtsyService.EtsyService etsyService)
+        //{
+        //    var r = await etsyService.Search("monstera deliciosa", 100, 0, 1);
 
 
-        }
+        //}
 
 
         //[Theory, Conventions]
